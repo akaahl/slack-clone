7961259397f77f93dcbd4699ce30daa22bf1def5
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-26 20:01:40 +0800

    Implement the Google Login functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,60 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
+import { auth, provider } from "../../firebase/firebase";
 
 function Login() {
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth.signInWithPopup(provider).catch((err) => alert(err.message));
+  };
+
   return (
     <LoginContainer>
-      <h2>Login</h2>
+      <LoginInnerContainer>
+        <img
+          src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+          alt="slack"
+        />
+
+        <h1>Sign In</h1>
+
+        <Button type="submit" onClick={signIn}>
+          Sign In with Google
+        </Button>
+      </LoginInnerContainer>
     </LoginContainer>
   );
 }
 
 export default Login;
 
-const LoginContainer = styled.div``;
+const LoginContainer = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: #f8f8f8;
+`;
+
+const LoginInnerContainer = styled.div`
+  padding: 100px;
+  background-color: white;
+  text-align: center;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
+  > img {
+    object-fit: contain;
+    height: 100px;
+    margin-bottom: 40px;
+  }
+
+  > button {
+    margin-top: 50px;
+    text-transform: inherit !important;
+    background-color: #0ab4ab;
+    color: white;
+    font-weight: 500;
+  }
+`;
